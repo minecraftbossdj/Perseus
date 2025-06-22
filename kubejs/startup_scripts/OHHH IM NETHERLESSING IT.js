64880d3968b2c06d01420fecbece7d66d1d668ef
riley <minecraftbossdj@gmail.com>
2025-06-21 20:21:03 -0400

    reverting because idk whats wrong

--- kubejs/startup_scripts/OHHH IM NETHERLESSING IT.js
+++
@@ -1,5 +0,0 @@
-StartupEvents.registry('item', event => {
- event.create('coal_rod').texture('netherless:item/coal_rod')
- event.create('netherite_fragment').texture('netherless:item/netherite_fragment')
- event.create('incomplete_blaze_rod', 'create:sequenced_assembly').texture('netherless:item/incomplete_blaze_rod')
-})
